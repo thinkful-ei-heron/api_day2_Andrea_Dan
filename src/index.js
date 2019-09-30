8e8f4b851c5a685b0ba2fe4f3d3156f4c33706cf
Andrea Bender <andreab.1726@gmail.com>
2019-09-30 12:59:09 -0600

    find and update problem

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@ import './index.css';
 import shoppingList from './shopping-list';
 
 const main = function() {
+  api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
+  console.dir(store.items);
+  const item = store.items[0];
+  console.log(store.items[0]);
+  console.log('current name: ' + item.name);
+  store.findAndUpdate(item.id, { name: 'foobar' });
+  console.log('new name: ' + item.name);
+
+  // api.createItem(newItemName)
+  // .then(res => res.json())
+  // .then((newItem) => {
+  //   store.addItem(newItem);
+  //   render();
+  // });
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
