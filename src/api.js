a0d6176c7f22d0be87b1170bfb4e7680887863a0
danWagar <dandwagar@gmail.com>
2019-09-30 09:08:33 -0700

    test api call

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/andreabender';
 
-const getItems() {
-  return Promise.resolve('A successful response!');
-}
+const getItems = function() {
+  //return Promise.resolve('A successful response!');
+  //console.log(`${BASE_URL}/items`);
+  return fetch(`${BASE_URL}/items`);
+};
 
 export default {
   getItems
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import $ from 'jquery';
-
+import api from './api';
 import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
 
 const main = function() {
-  fetch('https://thinkful-list-api.herokuapp.com/ei-student/items')
-    .then(res => res.json())
-    .then(data => console.log(data));
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
